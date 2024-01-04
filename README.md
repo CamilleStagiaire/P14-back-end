8271abfa5075ed0a3f8d4bd534396e8e8a437cdb
CamilleStagiaire <100408607+CamilleStagiaire@users.noreply.github.com>
2024-01-04 18:28:20 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Wealth Health
+
+# Faites passer une librairie jQuery vers React - Projet 14
+https://github.com/CamilleStagiaire/P14-back-end.git
+
+*Formation Développeur d'application - JavaScript React*
+
+# Etapes
+**installation et lancement du back-end:**  
+npm init -y  
+npm install express mongoose cors  
+
+**Configuration d'un serveur Express de base:**    
+
+**Connexion à MongoDB:**  
+
+**Demarrage du serveur:**  
+node server.js  
+
---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Connect to MongoDB
+mongoose.connect('mongodb://0.0.0.0/employeesDB');
+
+// Schema for the Employee
+const employeeSchema = new mongoose.Schema({
+  firstName: String,
+  lastName: String,
+  dateOfBirth: Date,
+  startDate: Date,
+  street: String,
+  city: String,
+  state: String,
+  zipCode: String,
+  department: String
+});
+
+// Model for the Employee
+const Employee = mongoose.model('Employee', employeeSchema);
+
+// Routes
+app.get('/api/employees', async (req, res) => {
+  const employees = await Employee.find();
+  res.json(employees);
+});
+
+app.post('/api/employees', async (req, res) => {
+  const newEmployee = new Employee(req.body);
+  await newEmployee.save();
+  res.status(201).json(newEmployee);
+});
+
+app.get('/', (req, res) => {
+    res.send('Bienvenue sur mon serveur API!');
+  });
+
+// Start the server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
